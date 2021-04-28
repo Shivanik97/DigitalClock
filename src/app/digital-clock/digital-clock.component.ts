fe23b29a0865b00799aa56d5be71091192c165e5
Shivanik1997 <shivani@fritsgo.com>
2021-04-28 14:59:58 +0530

    initial commit

---
+++ src/app/digital-clock/digital-clock.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-digital-clock',
+  templateUrl: './digital-clock.component.html',
+  styleUrls: ['./digital-clock.component.scss']
+})
+export class DigitalClockComponent implements OnInit {
+
+  private daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  private date = new Date();
+  public hour: any;
+  public minute: string;
+  public second: string;
+  public ampm: string;
+  public day: string;
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      const date = new Date();
+      this.updateDate(date);
+    }, 1000);
+    this.day = this.daysArray[this.date.getDay()];
+  }
+
+  private updateDate(date){
+    const hours = date.getHours();
+    this.ampm = hours >= 12 ? 'PM' : 'PM';
+    this.hour = hours % 12;
+    this.hour = this.hour ? this.hour: 12;
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+
+    const minutes = date.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes: minutes.toString();
+    
+    const seconds = date.getSeconds();
+    this.second = seconds < 10 ? '0' + seconds : seconds.toString();
+  }
+
+}
